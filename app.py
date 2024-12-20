8a52d1862bb9e68d23c374779a04e0572f4efa94
Ashish <khadka99ashish@gmail.com>
2024-12-20 17:26:51 -0600

    Loading the project to github.

---
+++ app.py
@@ -0,0 +1,161 @@
+import streamlit as st
+import pandas as pd
+from dbhelper import DB
+import plotly.graph_objects as go
+import plotly.express as px
+
+db = DB()
+
+st.sidebar.title('Flights Analytics')
+
+user_option = st.sidebar.selectbox('Menu',['OverView','Check Flights','Analytics'])
+
+if user_option == 'Check Flights':
+    st.title('Check Flights')
+
+    col1,col2 = st.columns(2)
+
+    city = db.fetch_city_names()
+
+    with col1:
+        source = st.selectbox('Source',sorted(city))
+    with col2:
+        destination = st.selectbox('Destination', sorted(city))
+
+    if st.button('Search'):
+        results = db.fetch_all_flights(source,destination)
+        # Define column names
+        columns = ["Airline", "Route", "Dep_Time", "Duration", "Price"]
+
+        # Convert result to DataFrame
+        if results:
+            df = pd.DataFrame(results, columns=columns)
+            st.dataframe(df)  # Display DataFrame with column names
+        else:
+            st.write("No flights found for the selected Source and Destination.")
+
+        #st.dataframe(results)
+
+elif user_option == 'Analytics':
+    airline, frequency = db.fetch_airline_frequency()
+    fig = go.Figure(
+        go.Pie(
+            labels=airline,
+            values=frequency,
+            hoverinfo="label+percent",
+            textinfo="value"
+        ))
+
+    st.subheader("Number of Flights Per Airlines")
+    st.plotly_chart(fig)
+
+    city, frequency = db.busy_airport()
+    colors = ['blue', 'green', 'orange', 'red', 'purple', 'pink', 'yellow', 'teal']
+    fig = go.Figure(
+        go.Bar(
+            x=city,
+            y=frequency,
+            text=frequency,
+            textposition='auto',
+            marker=dict(color=colors),
+            textfont=dict(  # Customize data label font
+                color="black",  # Font color for data labels
+                size=14,  # Font size for data labels
+                family="Arial"  # Font family for data labels
+            ),
+            hoverinfo="x+y"))
+
+    st.subheader("Busy Airport")
+    st.plotly_chart(fig)
+
+    date,frequency = db.daily_frequency()
+
+    # Custom colors for line and markers
+    line_color = "blue"
+    marker_color = "red"
+
+    # Create the line chart
+    fig = go.Figure(
+        go.Scatter(
+            x=date,  # X-axis categories
+            y=frequency,  # Y-axis values
+            mode='lines+markers+text',  # Display lines, markers, and text labels
+            text=frequency,  # Data labels
+            textposition='top center',  # Position data labels above the markers
+            line=dict(color=line_color, width=1),  # Line color and width
+            marker=dict(
+                color=marker_color,  # Marker color
+                size=10  # Marker size
+            ),
+            hoverinfo="x+y"  # Display city and frequency on hover
+        )
+    )
+
+    # Add a header and render the chart in Streamlit
+    st.subheader("Daily Flights")
+    st.plotly_chart(fig)
+
+
+    time, frequency, price = db.dep_time_and_price()
+
+    # Custom colors for line and markers
+    line_color = "green"
+    marker_color = "red"
+
+    # Create the line chart
+    fig = go.Figure(
+        go.Scatter(
+            x=time,  # X-axis categories
+            y=price,  # Y-axis values
+            mode='lines+markers+text',  # Display lines, markers, and text labels
+            text=frequency,  # Data labels
+            textposition='top center',  # Position data labels above the markers
+            line=dict(color=line_color, width=2),  # Line color and width
+            marker=dict(
+                color=marker_color,  # Marker color
+                size=10  # Marker size
+            ),
+            hoverinfo="x+y"  # Display city and frequency on hover
+        )
+    )
+    st.subheader("Time Vs Price")
+    st.plotly_chart(fig)
+
+
+else:
+    st.title("Flight Data Analysis Dashboard")
+
+    # Add a brief description
+    st.markdown("""
+        ### Welcome to the Flight Data Analysis Dashboard!
+
+        This project provides a comprehensive analysis of flight details, helping users gain valuable insights into flight patterns, pricing trends, and travel dynamics. The dataset includes crucial flight information such as:
+
+        - **Airline**: The carrier operating the flight.
+        - **Date of Journey**: The travel date, offering insights into seasonality and trends.
+        - **Source and Destination**: The cities of departure and arrival.
+        - **Route**: The flight path, including layovers.
+        - **Departure Time**: The time the flight departs, categorized into time slots like morning, afternoon, and evening.
+        - **Duration**: The total travel time.
+        - **Total Stops**: The number of layovers during the journey.
+        - **Price**: The ticket cost, enabling pricing pattern analysis.
+
+        ---
+
+        #### **Dashboard Features**
+        1. **Interactive Visualizations**: Analyze flight data through bar charts, line graphs, scatter plots, and more.
+        2. **Price Trends**: Explore how ticket prices vary based on factors like time of journey, airlines, and stops.
+        3. **Travel Patterns**: Identify peak travel times and popular routes.
+        4. **Duration Analysis**: Compare travel times across airlines and routes to find the most efficient flights.
+        5. **Filter and Search**: Customize your analysis by selecting specific airlines, travel dates, or destinations.
+
+        ---
+
+        #### **Objective**
+        This project aims to provide users with actionable insights to make informed decisions about flight bookings and understand market trends in the aviation industry.
+
+        Explore the data and uncover patterns that impact travel choices!
+        """)
+
+    # Add a footer or tagline
+    st.markdown("**Get started by selecting a Analytics or Check Flights!**")
---
+++ dbhelper.py
@@ -0,0 +1,150 @@
+import mysql.connector
+import os
+
+class DB:
+    def __init__(self):
+        # connect to the database
+
+        username = os.environ.get('user_name')
+        password = os.environ.get('user_password')
+
+        if not username or not password:
+            raise ValueError("Database username or password not set in environment variables.")
+
+
+        try:
+            self.conn = mysql.connector.connect(
+                host='database-olap.c5wkoy0cggj5.us-east-2.rds.amazonaws.com',
+                user= username,
+                password= password,
+                database='ashish'
+            )
+            self.mycursor = self.conn.cursor()
+            print('Connection established')
+        except:
+            print('Connection error')
+
+    def fetch_city_names(self):
+        if not self.mycursor:
+            raise ConnectionError("Database connection is not established.")
+
+
+        city = []
+        self.mycursor.execute("""
+        SELECT DISTINCT(Destination) FROM ashish.flight
+        UNION
+        SELECT DISTINCT(Source) FROM ashish.flight
+        """)
+
+        data = self.mycursor.fetchall()
+
+        for item in data:
+            city.append(item[0])
+
+        return city
+
+    def fetch_all_flights(self,source,destination):
+        if not self.mycursor:
+            raise ConnectionError("Database connection is not established.")
+
+
+        self.mycursor.execute("""
+        SELECT Airline,Route,Dep_Time,Duration,Price FROM flight
+        WHERE Source = '{}' AND Destination = '{}'
+        """.format(source,destination))
+
+        data = self.mycursor.fetchall()
+
+        return data
+
+    def fetch_airline_frequency(self):
+        if not self.mycursor:
+            raise ConnectionError("Database connection is not established.")
+
+
+        airline = []
+        frequency = []
+
+        self.mycursor.execute("""
+        SELECT Airline,COUNT(*) FROM flight
+        GROUP BY Airline
+        """)
+
+        data = self.mycursor.fetchall()
+
+        for item in data:
+            airline.append(item[0])
+            frequency.append(item[1])
+
+        return airline,frequency
+
+    def busy_airport(self):
+        if not self.mycursor:
+            raise ConnectionError("Database connection is not established.")
+
+
+        city = []
+        frequency = []
+
+        self.mycursor.execute("""
+        SELECT Source,COUNT(*) FROM (SELECT Source FROM flight
+							UNION ALL
+							SELECT Destination FROM flight) t
+        GROUP BY t.Source
+        ORDER BY COUNT(*) DESC
+        """)
+
+        data = self.mycursor.fetchall()
+
+        for item in data:
+            city.append(item[0])
+            frequency.append(item[1])
+
+        return city, frequency
+
+    def daily_frequency(self):
+        if not self.mycursor:
+            raise ConnectionError("Database connection is not established.")
+
+
+        date = []
+        frequency = []
+
+        self.mycursor.execute("""
+        SELECT Date_of_Journey,COUNT(*) FROM flight
+        GROUP BY Date_of_Journey
+        """)
+
+        data = self.mycursor.fetchall()
+
+        for item in data:
+            date.append(item[0])
+            frequency.append(item[1])
+
+        return date, frequency
+
+    def dep_time_and_price(self):
+        if not self.mycursor:
+            raise ConnectionError("Database connection is not established.")
+
+
+        if not self.mycursor:
+            raise ConnectionError("Database connection is not established.")
+
+        time = []
+        frequency = []
+        price = []
+        self.mycursor.execute("""
+        SELECT dep_time,COUNT(*),Price FROM flight
+            GROUP BY dep_time,Price
+            ORDER BY COUNT(*) DESC
+            LIMIT 20
+        """)
+        data = self.mycursor.fetchall()
+
+        for items in data:
+            time.append(items[0])
+            frequency.append(items[1])
+            price.append(items[2])
+
+        return time,frequency,price
